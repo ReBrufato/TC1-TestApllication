a781364993927fc02036c7e6e1d510efba55bdd3
kpi-jps <jps.spj@gmail.com>
2023-11-16 23:12:14 -0300

    refactor: It was creates a static reference for webdriver and actived a headless mode for it

--- testapppawnsandcare/src/test/java/Tests.java
+++ testapppawnsandcare/src/test/java/Tests.java
@@ -1,23 +1,37 @@
 import java.io.File;
+import java.time.Duration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxDriverService;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.GeckoDriverService;
 
 public class Tests {
 
+    private static String baseUrl = "http://localhost:3000";
+    private static File file = new File("src/test/resources/webdriver/geckodriver");
+    private static WebDriver driver = null;
+
+    @BeforeEach
+    public void getWebDriver() {
+        if (driver == null) {
+            FirefoxDriverService service = new GeckoDriverService.Builder().usingDriverExecutable(file).build();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("-headless");
+            driver = new FirefoxDriver(service, options);
+        }
+    }
+
     @Test
     @DisplayName("Testing the webdriver")
-    public void testingTheWebdriver() throws InterruptedException {
-        File file = new File("src/test/resources/webdriver/geckodriver");
-        FirefoxDriverService service = new GeckoDriverService.Builder().usingDriverExecutable(file).build();
-        WebDriver driver = new FirefoxDriver(service);
-        driver.get("http://localhost:3000"); 
-        Thread.sleep(1000);
-        driver.quit(); 
+    public void testingTheWebdriver() {
+        driver.get(baseUrl);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        driver.quit();
     }
 
 }
