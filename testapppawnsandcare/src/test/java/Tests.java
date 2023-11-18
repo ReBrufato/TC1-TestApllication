98252c58f5bc6ffd138abadb21823ca9ff8e1684
kpi-jps <jps.spj@gmail.com>
2023-11-18 10:16:02 -0300

    test: It was added the test method shouldRegisterAClient

--- testapppawnsandcare/src/test/java/Tests.java
+++ testapppawnsandcare/src/test/java/Tests.java
@@ -1,6 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.File;
 import java.time.Duration;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,11 +13,16 @@ import org.openqa.selenium.firefox.FirefoxDriverService;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.GeckoDriverService;
 
+import com.github.javafaker.Faker;
+
+import pageobjects.IndexPage;
+
 public class Tests {
 
     
     private static File file = new File("src/test/resources/webdriver/geckodriver");
     private static WebDriver driver = null;
+    private static Faker faker = new Faker();
 
     @BeforeEach
     public void getWebDriver() {
@@ -26,7 +34,23 @@ public class Tests {
         }
     }
 
+    @AfterEach
+    public void quitWebDriver() {
+        if (driver != null) driver.quit();
+    }
 
+    @Test
+    @DisplayName("shouldRegisterAClient")
+    public void shouldRegisterAClient() {
+        IndexPage indexPage = new IndexPage(driver);
+        String name = faker.name().fullName();
+        indexPage.registerClient(
+            name,
+            faker.idNumber().toString(),
+            faker.internet().emailAddress(),
+            faker.phoneNumber().toString());
+        assertEquals(indexPage.findClient(name), name);
+    }
 
 
 }
