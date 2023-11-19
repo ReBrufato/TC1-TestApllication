359b87dbf596c6cc6ec0e2c6d19d5779975143c0
kpi-jps <jps.spj@gmail.com>
2023-11-19 08:46:21 -0300

    refactor: It was refactored the constructor of IndexPage class and the method registerClient. Also it was created a method isClientNameInThePage

--- testapppawnsandcare/src/test/java/pageobjects/IndexPage.java
+++ testapppawnsandcare/src/test/java/pageobjects/IndexPage.java
@@ -15,44 +15,54 @@ public class IndexPage {
     private By searchInputBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[2]/input");
     private By searchBtnBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[1]/button");
     private By registerBtnBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div/button");
-    private By inputElementBy = By.className("sc-giAruI");
-    private By nameInTableBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/table/tbody/tr/th[2]/span");
+    private By inputElementsBy = By.className("sc-giAruI");
+    private By nameInTableBy = By
+            .xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/table/tbody/tr/th[2]/span");
     private By confirmBtnBy = By.xpath("/html/body/div[3]/div[2]/div/div/div[3]/button[2]");
+    private By tableBy = By.xpath("url");
     private By clientNamesBy = By.className("kPiqhn");
     private By coverLayerBy = By.className("sc-iCoHzw");
+
     public IndexPage(WebDriver driver) {
         this.driver = driver;
+        driver.get(url);
     }
 
     public void registerClient(String name, String doc, String email, String phone) {
-        driver.get(url);
+        
         WebElement registerBtn = new WebDriverWait(driver, Duration.ofSeconds(10)).until(
                 ExpectedConditions.elementToBeClickable(registerBtnBy));
         registerBtn.click();
         List<WebElement> inputElements = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
-            ExpectedConditions.presenceOfAllElementsLocatedBy(inputElementBy));
+                ExpectedConditions.presenceOfAllElementsLocatedBy(inputElementsBy));
         inputElements.get(0).sendKeys(name);
         inputElements.get(1).sendKeys(doc);
         inputElements.get(2).sendKeys(email);
         inputElements.get(3).sendKeys(phone);
         WebElement confirmBtn = new WebDriverWait(driver, Duration.ofSeconds(10)).until(
-            ExpectedConditions.presenceOfElementLocated(confirmBtnBy));
+                ExpectedConditions.presenceOfElementLocated(confirmBtnBy));
         confirmBtn.click();
     }
 
-    public String findClient(String name) {
-
-        WebElement searchInput = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
-            ExpectedConditions.visibilityOfElementLocated(searchInputBy));
-        searchInput.sendKeys(name);
-        WebElement searchBtn = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
-            ExpectedConditions.elementToBeClickable(searchBtnBy));
-        searchBtn.click();
-        WebElement nameInTable = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
-            ExpectedConditions.visibilityOfElementLocated(nameInTableBy));
-        return nameInTable.getText();
+    public Boolean isClientNameInThePage(String name) {
+        try {
+            return name.equals(findClientName(name));
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    
+    private String findClientName(String name) {
+
+        WebElement searchInput = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+                ExpectedConditions.visibilityOfElementLocated(searchInputBy));
+        searchInput.sendKeys(name);
+        WebElement searchBtn = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+                ExpectedConditions.elementToBeClickable(searchBtnBy));
+        searchBtn.click();
+        WebElement nameInTable = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+                ExpectedConditions.visibilityOfElementLocated(nameInTableBy));
+        return nameInTable.getText();
+    }
 
 }
