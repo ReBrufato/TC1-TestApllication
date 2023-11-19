7d27d7f951e9684fe62753825ce2d742d22c6bee
ReBrufato <brufato17.renan@gmail.com>
2023-11-19 17:37:53 -0300

    feat: created method addClient in class IndexPage

--- testapppawnsandcare/src/test/java/pageobjects/IndexPage.java
+++ testapppawnsandcare/src/test/java/pageobjects/IndexPage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class IndexPage {
     private WebDriver driver;
-    private final String pageUrl = "http://localhost:3000/";
+    private final String url = "http://localhost:3000/";
     private By searchInputBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[2]/input");
     private By searchBtnBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[1]/button");
     private By registerBtnBy = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div/button");
@@ -28,7 +28,7 @@ public class IndexPage {
     }
 
     public void registerClient(String name, String doc, String email, String phone) {
-        driver.get(pageUrl);
+        driver.get(url);
         
         WebElement registerBtn = new WebDriverWait(driver, Duration.ofSeconds(10)).until(
                 ExpectedConditions.elementToBeClickable(registerBtnBy));
@@ -53,7 +53,7 @@ public class IndexPage {
     }
 
     private String findClientName(String name) {
-        driver.get(pageUrl);
+        driver.get(url);
 
         WebElement searchInput = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
                 ExpectedConditions.visibilityOfElementLocated(searchInputBy));
@@ -67,7 +67,7 @@ public class IndexPage {
     }
 
     public String getPagePet(){
-        driver.get(pageUrl);
+        driver.get(url);
 
         final WebElement linkPet = new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div[1]/div[2]/div/div/a[2]")));
@@ -80,7 +80,7 @@ public class IndexPage {
     }
 
     public String getPageClientes(){
-        driver.get(pageUrl + "pets");
+        driver.get(url + "pets");
 
         final WebElement linkClient = new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div[1]/div[2]/div/div/a[1]")));
@@ -90,6 +90,34 @@ public class IndexPage {
                 .until(ExpectedConditions.titleIs("Paws & Care | Clientes"));
 
         return driver.getTitle();
+    }
+
+    public Integer addClient(String name, String doc, String email, String tel){
+        driver.get(url);
+
+        final WebElement buttonAddClient = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div/button")));
+
+        //verify how many lines had before
+        WebElement tableBody = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/table/tbody"));
+        Integer numberRowsBefore = tableBody.findElements(By.tagName("tr")).size();
+
+        buttonAddClient.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div/div")));
+
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/input")).sendKeys(name);
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/input")).sendKeys(doc);
+        driver.findElement(By.xpath("//*[@id=\"portal-modal\"]/div[2]/div/div/div[2]/div[3]/div[1]/div[2]/input")).sendKeys(email);
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div[3]/div[2]/div[2]/input")).sendKeys(tel);
+
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[3]/button[2]")).click();
+
+        //verified if row add
+        Integer numberRowsAfter = tableBody.findElements(By.tagName("tr")).size();
+
+        return numberRowsAfter - numberRowsBefore;
     }
 
 }
