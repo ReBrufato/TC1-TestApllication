315c75e425f604930ccba4a086b1f95a268479ca
kpi-jps <jps.spj@gmail.com>
2023-11-19 16:40:35 -0300

    feat: It was added to the project the WebDriverManager

--- testapppawnsandcare/src/test/java/Tests.java
+++ testapppawnsandcare/src/test/java/Tests.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.time.Duration;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,29 +16,26 @@ import org.openqa.selenium.firefox.GeckoDriverService;
 
 import com.github.javafaker.Faker;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pageobjects.IndexPage;
 
 public class Tests {
 
-    // private File linuxFile = newFile("src/test/resources/webdriver/linux/geckodriver");
-    private File file = new File("src/test/resources/webdriver/win/geckodriver.exe");
-    private WebDriver driver = null;
+    private WebDriver driver;
     private Faker faker = new Faker();
+
+    @BeforeAll
+    public static void setupWebDriver() {
+        WebDriverManager.firefoxdriver().setup();
+    }
 
     @BeforeEach
     public void getWebDriver() {
-        if (driver == null) {
-            FirefoxDriverService service = new GeckoDriverService.Builder().usingDriverExecutable(file).build();
-            //FirefoxOptions options = new FirefoxOptions();
-            //options.addArguments("-headless");
-            //driver = new FirefoxDriver(service, options);
-            driver = new FirefoxDriver(service);
-        }
+       driver = new FirefoxDriver();
     }
 
    @AfterEach
     public void quitWebDriver() {
-        if (driver != null)
             driver.quit();
     }
 
@@ -45,15 +43,13 @@ public class Tests {
     @DisplayName("Should register a client and search for his name in the page")
     public void shouldRegisterAClientAndSearchForHisNameInThePage() {
         IndexPage indexPage = new IndexPage(driver);
-        String name = faker.name().fullName();
-        
+        String name = faker.name().fullName();  
         indexPage.registerClient(
                 name,
                 faker.idNumber().toString(),
                 faker.internet().emailAddress(),
                 faker.phoneNumber().toString());
                 
-        assertThat(indexPage.isClientNameInThePage(name)).isTrue();
-         
+        assertThat(indexPage.isClientNameInThePage(name)).isTrue();   
     }
 }
