838d412f5748207848fba681c766a7b36c8e55aa
kpi-jps <jps.spj@gmail.com>
2023-11-20 00:13:28 -0300

    feat: It was added the method goToIndexPage in the PetPage class

--- testapppawnsandcare/src/test/java/pageobjects/PetPage.java
+++ testapppawnsandcare/src/test/java/pageobjects/PetPage.java
@@ -1,11 +1,19 @@
 package pageobjects;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PetPage {
     
     private WebDriver driver;
     private final String url = "http://localhost:3000/pets"; 
+
+    private By linkToIndexPageBy = By.xpath("/html/body/div[1]/div/div[1]/div[2]/div/div/a[1]");
 
     public PetPage(WebDriver driver) {
         this.driver = driver;
@@ -14,5 +22,12 @@ public class PetPage {
 
     public String getPageUrl() {
         return url;
+    }
+
+    public IndexPage goToIndexPage(WebDriver driver) {
+        WebElement linkToIndexPage = new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+                ExpectedConditions.presenceOfElementLocated(linkToIndexPageBy));
+        linkToIndexPage.click();
+        return new IndexPage(driver);
     }
 }
