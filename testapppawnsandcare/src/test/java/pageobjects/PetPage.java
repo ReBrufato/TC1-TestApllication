f8dea82cc6be81287f569422f98c442352a921e0
kpi-jps <jps.spj@gmail.com>
2023-11-20 00:02:40 -0300

    feat: It was added the constructor method and the method getPageUrl

--- testapppawnsandcare/src/test/java/pageobjects/PetPage.java
+++ testapppawnsandcare/src/test/java/pageobjects/PetPage.java
@@ -1,5 +1,18 @@
 package pageobjects;
 
+import org.openqa.selenium.WebDriver;
+
 public class PetPage {
     
+    private WebDriver driver;
+    private final String url = "http://localhost:3000/pets"; 
+
+    public PetPage(WebDriver driver) {
+        this.driver = driver;
+        driver.get(url);
+    }
+
+    public String getPageUrl() {
+        return url;
+    }
 }
