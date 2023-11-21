e3bdc6d4e52cde8e8dbceaf6501538837b24984e
ReBrufato <brufato17.renan@gmail.com>
2023-11-21 20:55:22 -0300

    test: Nested annotation added

--- testapppawnsandcare/src/test/java/Tests.java
+++ testapppawnsandcare/src/test/java/Tests.java
@@ -1,10 +1,7 @@
 
 import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -26,76 +23,85 @@ public class Tests {
     }
 
     @BeforeEach
-    public void getWebDriver() {
-        driver = new FirefoxDriver();
-    }
+    public void getWebDriver() {driver = new FirefoxDriver();}
 
     @AfterEach
     public void quitWebDriver() {
         driver.quit();
     }
 
-    @Test
-    @DisplayName("Should access the pet page from index page")
-    void shouldAccessThePetPageFromIndexPage() {
-        IndexPage indexPage = new IndexPage(driver);
-        PetPage petPage = indexPage.goToPetPage();
-        assertThat(petPage.getPageUrl()).isEqualTo(driver.getCurrentUrl());
+    @Nested
+    @DisplayName("Tests IndexPage")
+    class IndexPageTests{
+        @Test
+        @DisplayName("Should access the pet page from index page")
+        void shouldAccessThePetPageFromIndexPage() {
+            IndexPage indexPage = new IndexPage(driver);
+            PetPage petPage = indexPage.goToPetPage();
+            assertThat(petPage.getPageUrl()).isEqualTo(driver.getCurrentUrl());
+        }
+
+        @Test
+        @DisplayName("Should add a client")
+        public void shouldAddAClient() {
+            IndexPage indexPage = new IndexPage(driver);
+            int tableSizeBefore = indexPage.getClientTableBodySize();
+            indexPage.addClient(
+                    new ClientDTO(
+                            faker.name().fullName(),
+                            faker.idNumber().valid(),
+                            faker.internet().emailAddress(),
+                            faker.phoneNumber().phoneNumber()));
+            assertThat(indexPage.getClientTableBodySize()).isGreaterThan(tableSizeBefore);
+        }
+
+        @Test
+        @DisplayName("Should add a client and edit your data")
+        public void shouldAddAClientAndEditYourData() {
+            IndexPage indexPage = new IndexPage(driver);
+            ClientDTO inicialClientData = new ClientDTO(
+                    faker.name().fullName(),
+                    faker.idNumber().toString(),
+                    faker.internet().emailAddress(),
+                    faker.phoneNumber().toString());
+            indexPage.addClient(inicialClientData);
+            ClientDTO newClientData = new ClientDTO(
+                    faker.name().fullName(),
+                    faker.number().digits(10),
+                    faker.internet().emailAddress(),
+                    faker.phoneNumber().phoneNumber());
+            indexPage.editClient(newClientData);
+            assertThat(newClientData).isEqualTo(indexPage.getDataOfAddedClient());
+        }
+
+        @Test
+        @DisplayName("Should add a client and delete it")
+        public void shouldAddAClientAndDeleteIt() {
+            IndexPage indexPage = new IndexPage(driver);
+            int tableSizeBefore = indexPage.getClientTableBodySize();
+            indexPage.addClient(
+                    new ClientDTO(
+                            faker.name().fullName(),
+                            faker.idNumber().valid(),
+                            faker.internet().emailAddress(),
+                            faker.phoneNumber().phoneNumber()));
+            indexPage.deleteClient();
+            assertThat(indexPage.getClientTableBodySize()).isZero();
+        }
     }
 
-    @Test
-    @DisplayName("Should access the index page from pet page")
-    void shouldAccessTheIndexPageFromPetPage() {
-        PetPage petPage = new PetPage(driver);
-        IndexPage indexPage = petPage.goToIndexPage(driver);
-        assertThat(indexPage.getPageUrl()).isEqualTo(driver.getCurrentUrl());
+    @Nested
+    @DisplayName("Tests PetPage")
+    class PetPageTests{
+        @Test
+        @DisplayName("Should access the index page from pet page")
+        void shouldAccessTheIndexPageFromPetPage() {
+            PetPage petPage = new PetPage(driver);
+            IndexPage indexPage = petPage.goToIndexPage(driver);
+            assertThat(indexPage.getPageUrl()).isEqualTo(driver.getCurrentUrl());
+        }
+
     }
 
-    @Test
-    @DisplayName("Should add a client")
-    public void shouldAddAClient() {
-        IndexPage indexPage = new IndexPage(driver);
-        int tableSizeBefore = indexPage.getClientTableBodySize();
-        indexPage.addClient(
-                new ClientDTO(
-                        faker.name().fullName(),
-                        faker.idNumber().valid(),
-                        faker.internet().emailAddress(),
-                        faker.phoneNumber().phoneNumber()));
-        assertThat(indexPage.getClientTableBodySize()).isGreaterThan(tableSizeBefore);
-    }
 
-    @Test
-    @DisplayName("Should add a client and edit your data")
-    public void shouldAddAClientAndEditYourData() {
-        IndexPage indexPage = new IndexPage(driver);
-        ClientDTO inicialClientData = new ClientDTO(
-                faker.name().fullName(),
-                faker.idNumber().toString(),
-                faker.internet().emailAddress(),
-                faker.phoneNumber().toString());
-        indexPage.addClient(inicialClientData);
-        ClientDTO newClientData = new ClientDTO(
-                faker.name().fullName(),
-                faker.number().digits(10),
-                faker.internet().emailAddress(),
-                faker.phoneNumber().phoneNumber());
-        indexPage.editClient(newClientData);
-        assertThat(newClientData).isEqualTo(indexPage.getDataOfAddedClient());
-    }
-
-    @Test
-    @DisplayName("Should add a client and delete it")
-    public void shouldAddAClientAndDeleteIt() {
-        IndexPage indexPage = new IndexPage(driver);
-        int tableSizeBefore = indexPage.getClientTableBodySize();
-        indexPage.addClient(
-                new ClientDTO(
-                        faker.name().fullName(),
-                        faker.idNumber().valid(),
-                        faker.internet().emailAddress(),
-                        faker.phoneNumber().phoneNumber()));
-        indexPage.deleteClient();
-        assertThat(indexPage.getClientTableBodySize()).isZero();
-    }
 }
