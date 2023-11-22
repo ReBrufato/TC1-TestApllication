f4a4e2fad8b5e129e1617a5b236494bfce5c9e4c
ReBrufato <brufato17.renan@gmail.com>
2023-11-21 22:49:27 -0300

    refactor: change name atribute of race to breed

--- testapppawnsandcare/src/test/java/dtos/PetDTO.java
+++ testapppawnsandcare/src/test/java/dtos/PetDTO.java
@@ -7,16 +7,48 @@ public class PetDTO {
 
     private String type;
 
-    private String race;
+    private String breed;
 
     private String owner;
 
-    public PetDTO(String name, String type, String race) {this(name, type, race, null);}
+    public PetDTO(String name, String type, String breed) {this(name, type, breed, null);}
 
-    public PetDTO(String name, String type, String race, String owner) {
+    public PetDTO(String name, String type, String breed, String owner) {
         this.name = name;
         this.type = type;
-        this.race = race;
+        this.breed = breed;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
         this.owner = owner;
     }
 
@@ -25,11 +57,11 @@ public class PetDTO {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PetDTO petDTO = (PetDTO) o;
-        return Objects.equals(name, petDTO.name) && Objects.equals(type, petDTO.type) && Objects.equals(race, petDTO.race) && Objects.equals(owner, petDTO.owner);
+        return Objects.equals(name, petDTO.name) && Objects.equals(type, petDTO.type) && Objects.equals(breed, petDTO.breed) && Objects.equals(owner, petDTO.owner);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, type, race, owner);
+        return Objects.hash(name, type, breed, owner);
     }
 }
