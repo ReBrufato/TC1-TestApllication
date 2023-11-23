275f8da06c89b243810ce25105b9a1e0a1ae85a3
kpi-jps <jps.spj@gmail.com>
2023-11-22 22:16:49 -0300

    refactor: It was removed all set methods from PetDTO class and the property owner was renamed by ownerName

--- testapppawnsandcare/src/test/java/dtos/PetDTO.java
+++ testapppawnsandcare/src/test/java/dtos/PetDTO.java
@@ -4,52 +4,33 @@ import java.util.Objects;
 
 public class PetDTO {
     private String name;
-
     private String type;
-
     private String breed;
-
-    private String owner;
-
+    private String ownerName;
+    
     public PetDTO(String name, String type, String breed) {this(name, type, breed, null);}
 
-    public PetDTO(String name, String type, String breed, String owner) {
+    public PetDTO(String name, String type, String breed, String ownerName) {
         this.name = name;
         this.type = type;
         this.breed = breed;
-        this.owner = owner;
+        this.ownerName = ownerName;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String getBreed() {
         return breed;
     }
 
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
+    public String getOwnerName() {
+        return ownerName;
     }
 
     @Override
@@ -57,11 +38,11 @@ public class PetDTO {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PetDTO petDTO = (PetDTO) o;
-        return Objects.equals(name, petDTO.name) && Objects.equals(type, petDTO.type) && Objects.equals(breed, petDTO.breed) && Objects.equals(owner, petDTO.owner);
+        return Objects.equals(name, petDTO.name) && Objects.equals(type, petDTO.type) && Objects.equals(breed, petDTO.breed) && Objects.equals(ownerName, petDTO.ownerName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, type, breed, owner);
+        return Objects.hash(name, type, breed, ownerName);
     }
 }
